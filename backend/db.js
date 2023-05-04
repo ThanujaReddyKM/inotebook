ee2f6f7649dc7a2fa72bb5a8c3a9420de7789dce
ThanujaSorunReddy <thanuja.k@ibirdsservices.com>
2023-05-04 16:49:24 +0530

    Pushing

---
+++ backend/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+ const mongoURI = "mongodb://127.0.0.1:27017/"
+//const mongoURI = "mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+
+mongoose.set('strictQuery', false)
+
+// const connectToMongo =()=>{
+//     mongoose.connect(mongoURI, ()=>{
+//         console.log("connected to mongo successfully thanuja")
+//     })
+// }
+const connectToMongo = () => {
+    mongoose
+      .connect(mongoURI)
+      .then(() => console.log("connected to mongo successfully thanuja"))
+      .catch((err) => console.log(err));
+  };
+
+module.exports = connectToMongo;
+
