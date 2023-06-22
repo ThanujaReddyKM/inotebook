3c1b52d2e122f1ad33990120a77171e8ad54b73e
ThanujaSorunReddy <thanuja.k@ibirdsservices.com>
2023-06-22 10:58:05 +0530

    recallling_once

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,12 @@ const express = require('express')
 
 connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
 app.use(express.json());
+
+// app.get('/',(req,res)=>{
+//   res.send('Hello Welcome to new World')
+// })
 
 //Available Routes
 app.use('/api/auth',require ('./routes/auth'))
@@ -24,4 +28,39 @@ app.listen(port, () => {
 //     console.log(error);
 //   }
 // };
+
+
+
+
+// const connectToMongo = require('./db');
+// const express = require('express')
+
+// connectToMongo();
+// const app = express()
+// const port = 3000
+// //app.use(express.json());
+
+// app.get('/',(req,res)=>{
+//   res.send('Hello Welcome to new World')
+// })
+
+// //Available Routes
+// // app.use('/api/auth',require ('./routes/auth'))
+// // app.use('/api/notes',require ('./routes/notes'))
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+// // const start = async () => {
+// //   try {
+// //     await connectToMongo();
+// //     app.listen(port, () => {
+// //       console.log(`Example app listening at http://localhost:${port}`);
+// //     });
+// //   } catch (error) {
+// //     console.log(error);
+// //   }
+// // };
+
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,33 +1,123 @@
-const express = require('express');
-const User = require('../models/Users')
+const express = require("express");
+const User = require("../models/Users");
 const router = express.Router();
-const { validationResult, body } = require('express-validator');
+const { validationResult, body } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+var fetchuser = require('../middleware/fetchuser')
 
-//Create a user using: POST "/api/auth/createuser".  No login required
-router.post('/createuser',[
-    body('name','Enter a Valid Name').isLength({min:3}),
-    body('email','Enter a valid email').isEmail(),
-    body('password','Password length should be min 3').isLength({min:3}),
-],(req,res)=>{
+const JWT_SCCRET = "ThanujaSorunAmma";
+
+//Route 1:  Create a user using: POST "/api/auth/createuser".  No login required
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a Valid Name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password length should be min 3").isLength({ min: 3 }),
+  ],
+  async (req, res) => {
+    //If there are errors, return Bad Request and the errors.
     const errors = validationResult(req);
-    if(!errors.isEmpty())
-    {
-        return res.status(400).json({errors:errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name :req.body.name,
-        password:req.body.password,
-        email:req.body.email,
-    }).then(user => res.json(user))
-    .catch(err=> {console.log(err) 
-         res.json({error:'Please enter a unique value for email',message:err.message})})
 
+    //Check weather the user with this email exits already.
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry user with this email is already exits" });
+      }
 
-    // console.log(req.body);
-    // const user = User(req.body);
-    // user.save();
-     //res.send(req.body);
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      //create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
 
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SCCRET);
+      //console.log(jwtDate)
+
+      res.json({ authtoken });
+      //res.json({user})
+    } catch (error) {
+      //catch error
+      console.log(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+//Route 2: Aunthenticate a User Using :POST"/api/auth/login". No Login required.
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password Cannot be Blank").exists(),
+  ],
+  async (req, res) => {
+    //If there are errors, return Bad Request and the errors.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    console.log("email==>", req.body);
+    try {
+      let user = await User.findOne({ email });
+      console.log("user=>" + user);
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: "Please try to login with correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      console.log("password", +password);
+      console.log("user password", user.password);
+      console.log("passwordCompare=>" + passwordCompare);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ errors: "Please try to login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SCCRET);
+      res.json(authtoken);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+//ROUTE 3: Get loggedin user Deatails using:POST "/api/auth/getuser". Login Required
+router.post('/getuser',fetchuser, async(req,res)=>{
+    try {
+      const userId = req.user.id;
+        const user = await User.findById(userId).select("-password")
+        res.send(user);
+    } 
+    catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Server Error");
+        
+    }
 })
 
-module.exports = router
+
+module.exports = router;
